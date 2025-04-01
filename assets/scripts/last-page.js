7b755f13424f169ec53e873986ffbd2798d49555
Noemi Baglieri <noemibaglieri@gmail.com>
2025-04-01 14:43:31 +0200

    js

--- assets/scripts/last-page.js
+++ assets/scripts/last-page.js
@@ -1,19 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function () {
-  // Seleziona tutti gli elementi con la classe "star-off"
-  const starOff = document.querySelectorAll(".star-off");
-
-  // Controlla che la selezione abbia funzionato
-  console.log(starOff);
-
-  starOff.forEach((star, index) => {
-    star.addEventListener("click", function () {
-      for (let i = 0; i <= index; i++) {
-        starOff[i].src = "assets/star-on.svg";
-      }
-    });
-  });
-});
- */
 document.addEventListener("DOMContentLoaded", function () {
   const starOff = document.querySelectorAll(".star-off");
   starOff.forEach((star, index) => {
